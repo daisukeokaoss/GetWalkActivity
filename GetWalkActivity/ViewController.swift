a0cc6e8e57f9256af5ffe39f281c416e257a1058
Daisuke Oka <daisukeokahassou@gmail.com>
2018-09-30 11:26:16 +0900

    Initial Commit

---
+++ GetWalkActivity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetWalkActivity
+//
+//  Created by 岡大輔 on 2018/09/30.
+//  Copyright © 2018年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
