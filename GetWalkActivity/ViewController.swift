b7b8c12aab08f5b8d322d4127c31afc7dc76daa8
Daisuke Oka <daisukeokahassou@gmail.com>
2018-09-30 15:13:11 +0900

    implement step

--- GetWalkActivity/ViewController.swift
+++ GetWalkActivity/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import CoreMotion
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +17,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func getWalk(_ sender: Any) {
+        if(!CMPedometer.isStepCountingAvailable()) {
+            print("cannot get stepcount")
+        }
+        
+        let pedometer:CMPedometer = CMPedometer()
+        
+        let now = Date()
+        
+        let oneWeekAgo = Date(timeInterval: 604800, since: now as Date)
+        
+        pedometer.queryPedometerData(from: oneWeekAgo, to: now, withHandler: {(pedometerData:CMPedometerData!, error:NSError!) in
+            //print("\(pedometerData.numberOfSteps)") // 歩数
+            //print("\(pedometerData.distance)") // 距離
+            //print("\(pedometerData.floorsAscended)") // 上った回数
+            //print("\(pedometerData.floorsDescended)")
+            
+            
+            } as! CMPedometerHandler)
+        
 
+        
+    
+
+    }
+    
 }
 
